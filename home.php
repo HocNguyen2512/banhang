f7f229b18c7dd4ffab8c1f7af944d6c608a36cb3
HocNV <uchihaitachi25121999@gmail.com>
2021-07-12 00:09:33 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="banhang.css">
+</head>
+<body>'''
+<div>
+	<div class="div1">
+		<div class="div11">
+			<img src="https://i.pinimg.com/564x/ab/52/bc/ab52bcebeb73fcef4ea95acbc86840b4.jpg">
+
+		</div>
+		<div id="menu">
+			<ul>
+				<li><a href="">Trang chủ</a></li>
+				<li><a href="">Giới thiệu</a></li>
+				<li><a href="">Sản phẩm</a></li>
+				<li><a href="">Liên hệ</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="div">
+		<div class="div2">
+			<div class="div21">
+				<p> Thời Trang Nam</p>
+				<ul>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo Style</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Sơ mi Nam</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo thun Nam</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo khoác Nam</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Quần Jean Nam</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Giày dép Nam</a></li>
+				</ul>
+			</div>
+			<div class="div22">
+				<p> Thời Trang Nữ</p>
+				<ul>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo Style</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Sơ mi Nữ</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo thun Nữ</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Áo khoác Nữ</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Quần Jean Nữ</a></li>
+					<li><i class="fa fa-angle-right"></i><a href="">Váy Nữ</a></li><li><i class="fa fa-angle-right"></i><a href="">Giày dép Nữ</a></li>
+				</ul>
+			</div>
+			<div class="div23">
+				<p>Tìm Kiếm</p>
+				<input type="text" name="name" placeholder="Nhập từ khóa..."><br>
+				<input class="submit" type="submit" name="submit" value="tìm kiếm">
+			</div>
+		</div>
+		<div class="div3">
+			<div>
+				<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+			</div>
+			<div class="div31">
+				<div class="giua0">
+					<p>sản phẩm mới nhất</p>
+				</div>
+				<div class="giua">
+					<div class="giua1">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+					<div class="giua2">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+					<div class="giua3">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+				</div>
+				<div class="giua">
+					<div class="giua1">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+					<div class="giua2">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+					<div class="giua3">
+						<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png">
+						<p>aó sơ mi</p>
+						<p>giá:500000 vnđ</p>
+						<input type="submit" name="submit" value="chi tiết">
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="div4">
+			<div class="div41">
+				<p class="p"> Hỗ Trợ Trực Tuyến</p>
+				<p>hotline:0869374823</p>
+				<p>email:uchihaitachi@gmail.com</p>	
+			</div>
+			<div class="div41">
+				<p class="p"> Giỏ Hàng</p>
+				<p><i class="fa fa-shopping-cart"></i> My cart Style for Teen</p>
+			</div>
+			<div class="div41">
+				<p class="p">Tin tức thời trang</p>
+				<div class="div411">
+					<p>những đường kẻ kì diệu</p>
+					<div class="div4111">
+						<div>
+							<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png " width="100px">
+						</div>
+						<div>
+							<p>những đường kẻ kì diệu những đường kẻ kì diệu</p>
+						</div>
+					</div>	
+					<p><i class="fa fa-angle-double-right"></i>chi tiết <i class="fa fa-angle-double-left"></i></p>
+				</div>
+				<div class="div411">
+					<p>những đường kẻ kì diệu</p>
+					<div class="div4111">
+						<div>
+							<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png " width="100px">
+						</div>
+						<div>
+							<p>những đường kẻ kì diệu những đường kẻ kì diệu</p>
+						</div>
+					</div>	
+					<p><i class="fa fa-angle-double-right"></i>chi tiết <i class="fa fa-angle-double-left"></i></p>
+				</div>
+				<div class="div412">
+					<p>những đường kẻ kì diệu</p>
+					<div class="div4111">
+						<div>
+							<img src="https://img4.thuthuatphanmem.vn/uploads/2020/01/10/anh-nen-lien-minh-huyen-thoai-dep_055406078.png " width="100px">
+						</div>
+						<div>
+							<p>những đường kẻ kì diệu những đường kẻ kì diệu</p>
+						</div>
+					</div>	
+					<p><i class="fa fa-angle-double-right"></i>chi tiết <i class="fa fa-angle-double-left"></i></p>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+</body>
+</html>
